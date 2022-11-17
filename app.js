358933a9ebbfead60c1a850f93cf60fbb1b099f8
Marion Perrié <marion.perrie@gmail.com>
2022-11-17 15:57:14 +0100

    feat[js]: add post route "user signup"
    Create a model user.js and its userSchema
    Install mongoose-unique-validator to allow only 1 signup with same email
    Install bcrypt to hash password
    Create a controller user.js and its signup function
    Create a route user.js and implement the Post Signup route
    Update app.js to import and register the User router

--- app.js
+++ app.js
@@ -2,16 +2,37 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-//Connecting to Mongoose
+//Import the routers into the app
+const userRoutes = require('./routes/user');
+
+//Connect to Mongoose
 mongoose.connect('mongodb+srv://piiquanteadmin:carolina6reaper@piiquante.frqhwfm.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+//Parses incoming JSON requests and puts the parsed data in req.body
+app.use(express.json());
 
+//Allow the two apps to work together
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+//Test middlewares
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
+
+
+//Register routers
+app.use('/api/auth', userRoutes);
+
+
 
 module.exports = app;
