db7709a6863249d55109065379f025fdb724c4e6
Marion Perrié <marion.perrie@gmail.com>
2022-11-28 12:44:46 +0100

    fix[multer-config.js] : several problems
    Fix the MIMETYPES to fix the final extension to be jpg and not jpeg
    Fix the DateNow function that was missing the ()
    Add an underscore between the name of file and the date stamp
    Add an instruction to split the name file with the . and keep only
    the first part (the goal is to avoid having the intial extension type
    in the name of the file)

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 
 const MIME_TYPES = {
     'image/jpg' : 'jpg',
-    'image/jpeg' : 'jpeg',
+    'image/jpeg' : 'jpg',
     'image/png' : 'png'
 }
 
@@ -11,10 +11,12 @@ const storage = multer.diskStorage({
         callback(null, 'images')
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
+        var name = file.originalname.split('.')[0]
+        name = name.split(' ').join('_')
         const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now + '.' + extension)
+        callback(null, name + '_' + Date.now() + '.' + extension);
     }
 });
+
 
 module.exports = multer({storage: storage}).single('image');
